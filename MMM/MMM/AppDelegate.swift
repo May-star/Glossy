e44a8d64b7505de759ba0c1fb93dd75d6b444883
May-star <732871+May-star@users.noreply.github.com>
2018-05-22 00:03:34 +0900

    add a project

---
+++ MMM/MMM/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MMM
+//
+//  Created by May-star on 2018/05/22.
+//  Copyright © 2018年 May-star. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MMM/MMM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MMM
+//
+//  Created by May-star on 2018/05/22.
+//  Copyright © 2018年 May-star. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
